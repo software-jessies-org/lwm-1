28bf45079ba7bdfadfb29aae8d40e7d2fbb4b1d3
Phil Norman <philnorm@gmail.com>
2018-04-16 16:35:33 +0200

    Auto-detect when the screen changes size/shape (eg enabling an external screen on a laptop), and reinitialise the screens, move windows so they're still visible, etc.

--- lwm.c
+++ lwm.c
@@ -33,6 +33,7 @@
 #include <X11/Xlib.h>
 #include <X11/Xos.h>
 #include <X11/Xutil.h>
+#include <X11/extensions/Xrandr.h>
 
 #include "lwm.h"
 
@@ -77,6 +78,9 @@ char *argv0;
 
 static void initScreens(void);
 static void initScreen(int);
+
+static void rrScreenChangeNotify(XEvent *ev);
+static void rrNotify(XEvent *ev);
 
 /*ARGSUSED*/
 extern int main(int argc, char *argv[]) {
@@ -130,7 +134,7 @@ extern int main(int argc, char *argv[]) {
   _mozilla_url = XInternAtom(dpy, "_MOZILLA_URL", False);
 
   motif_wm_hints = XInternAtom(dpy, "_MOTIF_WM_HINTS", False);
-
+  
   ewmh_init();
 
   /*
@@ -176,7 +180,16 @@ extern int main(int argc, char *argv[]) {
   initScreens();
   ewmh_init_screens();
   session_init(argc, argv);
-
+  
+  // Do we need to support XRandR?
+  int rr_event_base, rr_error_base;
+  Bool have_rr = XRRQueryExtension(dpy, &rr_event_base, &rr_error_base);
+  if (have_rr) {
+    for (int i = 0; i < screen_count; i++) {
+      XRRSelectInput(dpy, screens[i].root, RRScreenChangeNotifyMask);
+    }
+  }
+  
   /* See if the server has the Shape Window extension. */
   shape = serverSupportsShapes();
 
@@ -207,7 +220,13 @@ extern int main(int argc, char *argv[]) {
         while (XPending(dpy)) {
           XEvent ev;
           XNextEvent(dpy, &ev);
-          dispatch(&ev);
+          // xrandr notifications have arbitrary numbers, so check for them
+          // before trying the static selection.
+          if (ev.type == rr_event_base + RRScreenChangeNotify) {
+            rrScreenChangeNotify(&ev);
+          } else {
+            dispatch(&ev);
+          }
         }
       }
       if (ice_fd > 0 && FD_ISSET(ice_fd, &readfds)) {
@@ -218,6 +237,132 @@ extern int main(int argc, char *argv[]) {
   // Someone hit us with a SIGHUP: better exec ourselves to force a config
   // reload and cope with changing screen sizes.
   execvp(argv0, argv);
+}
+
+// scaleSize scales *size so that it occupies the same proportion of 'news' as
+// it did 'olds' (these are new and old screen sizes in the same dimension),
+// with the caveat that any change will be made in multiples of inc.
+static int scaleSize(int olds, int news, int size, int inc) {
+  int add = ((size * news) / olds) - size;
+  // 'add' can, of course, be negative. That's fine.
+  add -= (add % inc);
+  return size + add;
+}
+
+// moveOrChangeSize takes in an old and new size (olds, news) describing the
+// change in one screen dimension (width or height). It takes pos, which is the
+// corresponding left or top location on that dimension, and size which is the
+// extent of the window in that dimension. Additionally inc describes the
+// increment in pixels of window size.
+// On return, *pos and *size are updated to represent a 'nice' positioning in
+// the new size.
+// In general, we try not to change the size, but rather shift the window
+// towards its closest screen edge proportionally to the size of the screen.
+// Thus, if a window started off 5% of the way across the screen from the left,
+// it will still be 5% across from the left on return.
+// On the other hand, if we detect that the window is trying to take up all the
+// screen space in some dimension (eg top and bottom of the window are within
+// 5% of the screen height of the top and bottom edges of the screen), we grow
+// the window in that dimension to ensure it still takes up the same proportion
+// of the screen size.
+static void moveOrChangeSize(int olds, int news, int *pos, int *size, int inc) {
+  if (inc < 1) {
+    inc = 1;
+  }
+  int opos = *pos;
+  int osize = *size;
+  // For clarity, comments assume we're talking about the X dimension. This is
+  // not necessarily the case, but it's easier to visualise if we pick one
+  // dimension.
+  int nearDist = *pos;                 // Distance from left edge of screen.
+  int farDist = olds - (*pos + *size); // Distance from right edge of screen.
+  Bool nearClose = (nearDist * 20 < olds); // Very close to left edge?
+  Bool farClose = (farDist * 20 < olds);   // Very close to right edge?
+
+  if (nearClose && farClose) {
+    // Window is full width; scale it up or down keeping the left and right
+    // edges the same distance from the screen edge.
+    *size += ((news - olds) / inc) * inc;
+  } else {
+    // If we're not scaling the window because it's full-screen, then we need to
+    // check its size to ensure it doesn't exceed the new screen size, and scale
+    // it down appropriately if it does. The general rule we'll apply is that if
+    // the screen is shrinking, and the new window size is 90% of the screen
+    // size
+    // or larger, then we'll scale it proportionally.
+    if (news < olds && (*size * 10 / 9) > news) {
+      // Too big; scale down.
+      *size = scaleSize(olds, news, *size, inc);
+    }
+    // The window size is going to be alright now, but how we move the window
+    // depends on its original position. If it was close to the left or right
+    // edges, it will follow them; if it was somewhere floating in the middle
+    // ground, we will move the window such that its central point will occupy
+    // the same proportional location of the screen, but adjusting to stop the
+    // window edge going off the side.
+    if (nearClose) {
+      *pos = (*pos * news) / olds;
+    } else if (farClose) {
+      farDist = (farDist * news) / olds;
+      *pos = news - farDist - *size;
+    } else {
+      *pos = (*pos * (news - *size)) / (olds - *size);
+    }
+  }
+}
+
+static void rrScreenChangeNotify(XEvent *ev) {
+  XRRScreenChangeNotifyEvent *rrev = (XRRScreenChangeNotifyEvent *)ev;
+  for (int i = 0; i < screen_count; i++) {
+    if (screens[i].root != rrev->root) {
+      continue;
+    }
+    const int oScrWidth = screens[i].display_width;
+    const int oScrHeight = screens[i].display_height;
+    const int nScrWidth = rrev->width;
+    const int nScrHeight = rrev->height;
+    // We've found the right screen. Refresh our idea of its size.
+    // Note that we don't call DisplayWidth() and DisplayHeight() because they
+    // seem to always return the original size, while the change notify event
+    // has the updated size.
+    if (oScrWidth == nScrWidth && oScrHeight == nScrHeight) {
+      return;  // Don't process the same event multiple times.
+    }
+    screens[i].display_width = nScrWidth;
+    screens[i].display_height = nScrHeight;
+    // Now, go through the windows and adjust their sizes and locations to conform to the new screen layout.
+    for (Client *c = client_head(); c; c = c->next) {
+      int x = c->size.x;
+      int y = c->size.y;
+      const int oldx = x;
+      const int oldy = y;
+      const int oldw = c->size.width;
+      const int oldh = c->size.height;
+
+      moveOrChangeSize(oScrWidth, nScrWidth, &x, &(c->size.width),
+                       c->size.width_inc);
+      moveOrChangeSize(oScrHeight, nScrHeight, &y, &(c->size.height),
+                       c->size.height_inc);
+      Edge backup = interacting_edge;
+      interacting_edge = ENone;
+      // Note: the only reason this doesn't crash (due to the last two args
+      // being 0) is that dx and dy are only used when edge != ENone.
+      // You have been warned.
+      Client_MakeSane(c, ENone, &x, &y, 0, 0);
+      interacting_edge = backup;
+      XMoveResizeWindow(dpy, c->parent, c->size.x, c->size.y - titleHeight(),
+                        c->size.width, c->size.height + titleHeight());
+      if (c->size.width == oldw && c->size.height == oldh) {
+        if (c->size.x != oldx || c->size.y != oldy) {
+          sendConfigureNotify(c);
+        }
+      } else {
+        XMoveResizeWindow(dpy, c->window, border, border + titleHeight(),
+                          c->size.width - 2 * border,
+                          c->size.height - 2 * border);
+      }
+    }
+  }
 }
 
 void sendConfigureNotify(Client *c) {
